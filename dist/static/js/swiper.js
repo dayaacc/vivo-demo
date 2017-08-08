94aabcdab1acde5385b8903004f5925d6ce2bbca
liumian <597681301@qq.com>
2017-08-08 21:08:44 +0800

    first commit

---
+++ dist/static/js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.slider .swiper-container', {
+        pagination: '.slider .swiper-pagination',
+        paginationClickable: true,
+        loop:true,
+        autoplay: 4000,
+        autoplayDisableOnInteraction: false
+    });
+var myswiper = new Swiper('.s-slider .swiper-container', {
+    pagination: '.s-slider .swiper-pagination',
+    paginationClickable: true,
+    loop:true,
+});
